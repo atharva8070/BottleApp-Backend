c71e2c1dda89fa800da9f610b46a611ad9972f9c
durgeshahire07 <durgeshahire07@gmail.com>
2023-06-21 23:24:28 +0530

    feat: :sparkles: added BottleCounter & modified app.py

--- app.py
+++ app.py
@@ -1,29 +1,20 @@
-from flask import Flask, request, jsonify
+import sys
+import os
+from flask import Flask, request
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/utils')
+from utils.BottleCounter import count_bottles
 
 app = Flask(__name__)
 
 @app.route('/process-image', methods=['POST'])
 def process_image():
-    try:
-        # Retrieve the image file from the request
-        image_file = request.files['image']
-        
-        # Process the image using your Python code
-        bottle_count = process_image_function(image_file)
-        
-        # Return the bottle count as a response
-        response = {'count': bottle_count}
-        return jsonify(response), 200
-    
-    except KeyError:
-        # Handle the case where the 'image' file is not found in the request
-        return 'No image file found', 400
+    # Retrieve the image file from the request
+    image_file = request.files['image']
 
-def process_image_function(image_file):
-    # Add your image processing code here
-    # ...
-    # Return the bottle count after processing the image
-    return 10  # Replace with your actual bottle count
+    # Process the image using the count_bottles function
+    bottle_count = count_bottles(image_file)
 
+    # Return the bottle count as a response
+    return {'count': bottle_count}
 if __name__ == '__main__':
     app.run(debug=True)
